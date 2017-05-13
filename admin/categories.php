6510962d8c9565e24eb461568c95dbe72dcddb60
Abhishar Sinha <abhishar.sinha@gmail.com>
2017-05-13 21:38:05 +0530

    Update categories.php

--- admin/categories.php
+++ admin/categories.php
@@ -1,5 +1,5 @@
 <?php
-  require_once $_SERVER['DOCUMENT_ROOT'].'/ecom/core/init.php';
+  require_once '../core/init.php';
   session_start();
   include 'includes/head.php';
   include 'includes/navigation.php';
